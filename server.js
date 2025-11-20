d622b4d9c31d1415620cb3a7936777f3acfa0222
kaushikazala <kaushikazala4@gmail.com>
2025-11-20 14:20:21 +0530

    backend

---
+++ server.js
@@ -0,0 +1,9 @@
+require('dotenv').config()
+const app = require('./src/app')
+const connectDB = require('./src/db/db')
+
+connectDB()
+
+app.listen( process.env.PORT,()=>{
+    console.log(`server is running on port ${process.env.PORT}`)
+})
